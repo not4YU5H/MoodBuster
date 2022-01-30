e9f7210995ebc8095797bdf1b21e28ce136c80f7
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2022-01-30 15:15:54 +0530

    Complete code added, some tweaks needed

---
+++ test.py
@@ -0,0 +1,160 @@
+from emotion_recognition import EmotionRecognizer
+
+import pyaudio
+import os
+import wave
+from sys import byteorder
+from array import array
+from struct import pack
+from sklearn.ensemble import GradientBoostingClassifier, BaggingClassifier
+
+from utils import get_best_estimators
+
+THRESHOLD = 500
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 16000
+
+SILENCE = 30
+
+def is_silent(snd_data):
+    return max(snd_data) < THRESHOLD
+
+def normalize(snd_data):
+    MAXIMUM = 16384
+    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
+
+    r = array('h')
+    for i in snd_data:
+        r.append(int(i*times))
+    return r
+
+def trim(snd_data):
+    def _trim(snd_data):
+        snd_started = False
+        r = array('h')
+
+        for i in snd_data:
+            if not snd_started and abs(i)>THRESHOLD:
+                snd_started = True
+                r.append(i)
+
+            elif snd_started:
+                r.append(i)
+        return r
+
+    snd_data = _trim(snd_data)
+
+    snd_data.reverse()
+    snd_data = _trim(snd_data)
+    snd_data.reverse()
+    return snd_data
+
+def add_silence(snd_data, seconds):
+    r = array('h', [0 for i in range(int(seconds*RATE))])
+    r.extend(snd_data)
+    r.extend([0 for i in range(int(seconds*RATE))])
+    return r
+
+def record():
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=1, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK_SIZE)
+
+    num_silent = 0
+    snd_started = False
+
+    r = array('h')
+
+    while 1:
+        # little endian, signed short
+        snd_data = array('h', stream.read(CHUNK_SIZE))
+        if byteorder == 'big':
+            snd_data.byteswap()
+        r.extend(snd_data)
+
+        silent = is_silent(snd_data)
+
+        if silent and snd_started:
+            num_silent += 1
+        elif not silent and not snd_started:
+            snd_started = True
+
+        if snd_started and num_silent > SILENCE:
+            break
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = normalize(r)
+    r = trim(r)
+    r = add_silence(r, 0.5)
+    return sample_width, r
+
+def record_to_file():
+    CHUNK = 1024 
+    FORMAT = pyaudio.paInt16 #paInt8
+    CHANNELS = 1 
+    RATE = 16000 #sample rate
+    RECORD_SECONDS = 4
+    WAVE_OUTPUT_FILENAME = "output10.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK) #buffer
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data) # 2 bytes(16 bits) per channel
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+def get_estimators_name(estimators):
+    result = [ '"{}"'.format(estimator.__class__.__name__) for estimator, _, _ in estimators ]
+    return ','.join(result), {estimator_name.strip('"'): estimator for estimator_name, (estimator, _, _) in zip(result, estimators)}
+
+
+
+if __name__ == "__main__":
+    estimators = get_best_estimators(True)
+    estimators_str, estimator_dict = get_estimators_name(estimators)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--emotions", default="sad,neutral,happy")
+    parser.add_argument("-m", "--model", help=
+                                        """
+                                        The model to use, 8 models available are: {},
+                                        default is "BaggingClassifier"
+                                        """.format(estimators_str), default="BaggingClassifier")
+
+
+    # Parse the arguments passed
+    args = parser.parse_args()
+
+    features = ["mfcc", "chroma", "mel"]
+    detector = EmotionRecognizer(estimator_dict[args.model], emotions=args.emotions.split(","), features=features, verbose=0)
+    detector.train()
+    print("Test accuracy score: {:.3f}%".format(detector.test_score()*100))
+    print("Please talk")
+    
+    record_to_file()
+    result = detector.predict("output10.wav")
+    print(result)
